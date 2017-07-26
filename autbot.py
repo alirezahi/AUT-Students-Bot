e864bd1c23f5fec6b8616faec5cf0ec950ee0d98
Alireza <alireza97hi@gmail.com>
2017-07-26 17:23:41 +0430

    std added to gitignore

--- autbot.py
+++ autbot.py
@@ -1,6 +1,9 @@
 import telebot
 from config import *
 from pymongo import MongoClient
+import urllib
+from urllib import request
+import json
 
 telebot = telebot.TeleBot(api_key)
 
@@ -25,5 +28,15 @@ def some_function(message):
     for res in result:
         print(res)
         telebot.reply_to(message, res['name'], reply_markup=markup)
+
+@telebot.message_handler(content_types=['photo'])
+def some_function_second(message):
+    file_id = message.photo[-1]
+    print(file_id)
+    result = urllib.request.urlopen('https://api.telegram.org/bot'+api_key+'/getFile?file_id='+file_id.file_id).read().decode('utf-8')
+    result = json.loads(result)
+    print(result['result']['file_path'])
+    urllib.request.urlretrieve('https://api.telegram.org/file/bot'+api_key+'/'+result['result']['file_path'],result['result']['file_path'].split('/')[1])
+    telebot.reply_to(message, 'yeap yeap photo', reply_markup=markup)
 
 telebot.polling()
